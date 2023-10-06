3472ece9a7f4dc640b526972a8e49fa070d63b0d
Andrew Dawes <andrewjamesdawes@gmail.com>
2023-10-06 12:27:13 +0000

    Revert "Use passed value"
    This reverts commit dc0db937bf259b6d0e27031f0b3a6fde3c96b64e.

--- src/components/FloatInput.tsx
+++ src/components/FloatInput.tsx
@@ -22,21 +22,21 @@ export default function FloatInput({
 	value: string;
 	onChange: (value: string) => void;
 }) {
-	// const [internalValue, setInternalValue] = useState(
-	// 	formatStringFloatValue(value)
-	// );
+	const [internalValue, setInternalValue] = useState(
+		formatStringFloatValue(value)
+	);
 	const [valueChangeTimeout, setValueChangeTimeout] =
 		useState<NodeJS.Timeout>();
-	// useEffect(() => {
-	// 	setInternalValue(formatStringFloatValue(value));
-	// }, [value]);
+	useEffect(() => {
+		setInternalValue(formatStringFloatValue(value));
+	}, [value]);
 	return (
 		<input
 			type="text"
 			name={name}
 			pattern="[0-9]*[.]?[0-9]{0,2}"
 			title="Enter a valid number with up to 2 decimal places."
-			value={value}
+			value={internalValue}
 			onChange={(e) => {
 				const periodCount = e.target.value.split(".").length - 1;
 				// Don't allow more than 8 numbers - decimal doesn't count
@@ -47,14 +47,13 @@ export default function FloatInput({
 					return;
 				}
 				const newValue = e.target.value;
-				onChange(newValue);
-				// setInternalValue(newValue);
+				setInternalValue(newValue);
 				clearTimeout(valueChangeTimeout);
 				// queue up a new timeout to round the value to 2 decimal places
 				setValueChangeTimeout(
 					setTimeout(() => {
 						const cleanNewValue = formatStringFloatValue(newValue);
-						// setInternalValue(cleanNewValue);
+						setInternalValue(cleanNewValue);
 						onChange(cleanNewValue);
 					}, 350)
 				);
@@ -62,7 +61,7 @@ export default function FloatInput({
 			// @todo - may need to be careful this doesn't cause some kind of loop
 			onBlur={(e) => {
 				const cleanNewValue = formatStringFloatValue(e.target.value);
-				// setInternalValue(cleanNewValue);
+				setInternalValue(cleanNewValue);
 				onChange(cleanNewValue);
 			}}
 		></input>
