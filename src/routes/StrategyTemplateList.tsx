3136805e03b177316b6952ba4b0f1496303041ad
Andrew Dawes <andrewjamesdawes@gmail.com>
2024-03-03 21:34:12 +0000

    Add StrategyTemplateList route

---
+++ src/routes/StrategyTemplateList.tsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useContext, useState } from "react";
+// import { ToggleState } from "../Interfaces/settings";
+import { Client as BlackdogConfiguratorClient } from "@umerx/umerx-blackdog-configurator-client-typescript";
+// import Toggle from "../components/Toggle";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus";
+import {
+	// Link,
+	// useLoaderData,
+	// LoaderFunction,
+	useParams,
+} from "react-router-dom";
+// import { faCheck } from "@fortawesome/free-solid-svg-icons/faCheck";
+// import { faTimes } from "@fortawesome/free-solid-svg-icons/faTimes";
+import BreadcrumbsContext from "../components/BreadcrumbsContext";
+import { StrategyGetSingleResponseBodyData } from "@umerx/umerx-blackdog-configurator-types-typescript/build/src/strategy";
+import z from "zod";
+interface StrategyTemplateListProps {
+	blackdogConfiguratorClient: BlackdogConfiguratorClient.Client;
+}
+
+// const toggleStateDisplays = {
+// 	[ToggleState.on]: (
+// 		<FontAwesomeIcon
+// 			icon={faCheck}
+// 			className="text-sm transition-bg duration-1000"
+// 		/>
+// 	),
+// 	[ToggleState.off]: (
+// 		<FontAwesomeIcon
+// 			icon={faTimes}
+// 			className="text-sm transition-bg duration-1000"
+// 		/>
+// 	),
+// };
+
+interface StrategyTemplateListParams {
+	strategyId: number;
+}
+
+const StrategyTemplateListParamsExpected = z.object({
+	strategyId: z.string().regex(/^\d+$/),
+});
+
+function StrategyTemplateListParamsFromRaw(
+	raw: any
+): StrategyTemplateListParams {
+	const parsed = StrategyTemplateListParamsExpected.parse(raw);
+	return {
+		strategyId: parseInt(parsed.strategyId, 10),
+	};
+}
+
+const StrategyTemplateList: React.FC<StrategyTemplateListProps> = ({
+	blackdogConfiguratorClient,
+}) => {
+	const params = useParams();
+	const { strategyId } = StrategyTemplateListParamsFromRaw(params);
+	const [strategy, setStrategy] =
+		useState<StrategyGetSingleResponseBodyData | null>(null);
+	const { setBreadcrumbs } = useContext(BreadcrumbsContext);
+	useEffect(() => {
+		if (!strategy) {
+			return;
+		}
+		setBreadcrumbs([
+			{
+				label: "Home",
+				path: "",
+			},
+			{
+				label: "Strategies",
+				path: "strategy",
+			},
+			{
+				label: `${strategy.title}`,
+				path: `strategy/${strategy.id}`,
+			},
+			{
+				label: "Templates",
+				path: "strategyTemplate",
+			},
+		]);
+	}, [strategy]);
+	useEffect(() => {
+		(async () => {
+			const strategy = await blackdogConfiguratorClient
+				.strategy()
+				.getSingle({ id: strategyId });
+			setStrategy(strategy);
+		})();
+	}, [strategyId]);
+
+	if (!strategy) {
+		return <div>Loading...</div>;
+	}
+	switch (strategy.strategyTemplateName) {
+		case "SeaDogDiscountScheme":
+			return <div>Sea</div>;
+		default:
+			return <div>Unknown strategy template</div>;
+	}
+};
+
+export default StrategyTemplateList;
