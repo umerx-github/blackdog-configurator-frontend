9c4c420e2fca5fe5bc812f1fae4bc744cbc81452
Missie Dawes <missiedawes@gmail.com>
2024-03-10 14:38:19 +0000

    Make logo navigate to home

--- src/components/BlackdogHeader.tsx
+++ src/components/BlackdogHeader.tsx
@@ -1,20 +1,26 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDog } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const BlackDogHeader: React.FC = () => {
 	return (
 		<div className="blackdog-header">
-			<div className="flex justify-between">
-				<h3 className="text-2xl orbitron">
-					<span className="font-black">
-						BL
-						<FontAwesomeIcon icon={faDog} className="logo-dog" />
-						CK
-					</span>
-					<span className="font-normal">DOG</span>
-				</h3>
-			</div>
+			<Link to="/">
+				<div className="flex justify-between">
+					<h3 className="text-2xl orbitron">
+						<span className="font-black">
+							BL
+							<FontAwesomeIcon
+								icon={faDog}
+								className="logo-dog"
+							/>
+							CK
+						</span>
+						<span className="font-normal">DOG</span>
+					</h3>
+				</div>
+			</Link>
 		</div>
 	);
 };
